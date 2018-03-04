83350fe559003109c1a9bac2c6cd3c32759a2b52
jayveyor <jayveyor@gmail.com>
2018-03-03 22:21:16 -0500

    fixing some js stuff
    hopefully

--- scripts.js
+++ scripts.js
@@ -75,40 +75,6 @@ $('.reload').on('click', function () {
     window.location.reload();
 });
 
-
-//Quiz page js actions
-
-//functions to create a 7 digit number to add to needed prefix for IMDB api
-//Need to create an array that could have 0's preceeding a number up to 7 sig. digits
-
-//pick a random number between 0 - 9
-
-const random = function () {
-    let random = Math.floor(Math.random() * 9);
-    return random;
-}
-
-//function for creating array of 7 indexes with 7 random numbers generated
-const numGen = function () {
-    let numberInput = [];
-    for (let i = 0; i < 7; i++) {
-        numberInput.push(random());
-    }
-    return numberInput;
-}
-
-//function for creating 7 digit string to put into imdb database
-const inputString = function () {
-    let stringInput = '';
-
-    numGen().forEach(function (data) {
-        stringInput = stringInput + data;
-    });
-
-    return stringInput;
-}
-
-
 for (let i = 0; i < 4; i++) {
     movieCall();
 
@@ -126,46 +92,32 @@ for (let i = 0; i < 4; i++) {
             }
         }).then(function (res) {
             //check output and print to screen
-            if (responseCheck(res) == 'True') {
+            
                 ifPosterExist(res);
-
-            } else {
-                movieCall();
-            };
-
         });
 
     }
 
-    //functions to test api data
-
-    const responseCheck = function (data) {
-        return data.Response;
-    }
-
-
     const ifPosterExist = function (data) {
-        if (data.Poster == "N/A" || data.Type !== "movie" || data.Language !== "English" || data.Title == "N/A" || data.Director == "N/A" || data.Year == "N/A") {
-            movieCall();
-        } else {
-
+       
+        console.log('hello')
             //adds poster to container
-            $(`.poster${i}`).attr('src', `${data.Poster}`);
+            $(`.poster1`).attr('src', `${data.Poster}`);
 
             //add title
-            $(`.title${i}`).append(`<h4 class="title${i}">${data.Title}</h4>`);
+            $(`.title1`).append(`<h4 class="title1">${data.Title}</h4>`);
 
             //add director
-            $(`.director${i}`).append(`<h4 class="director${i}">${data.Director}</h4>`);
+            $(`.director1`).append(`<h4 class="director1">${data.Director}</h4>`);
 
             //add year
-            $(`.year${i}`).append(`<h4 class="year${i}">${data.Year}</h4>`);
+            $(`.year1`).append(`<h4 class="year1">${data.Year}</h4>`);
 
             //add runtime
-            $(`.runtime${i}`).append(`<h4 class="runtime${i}">${data.Runtime}</h4>`);
+            $(`.runtime1`).append(`<h4 class="runtime1">${data.Runtime}</h4>`);
 
 
-        };
+        
     }
 
 }
